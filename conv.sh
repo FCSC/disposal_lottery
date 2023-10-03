8e1d786e97091b9e937824904cc93e1c037c043f
n-ando <Noriaki.Ando@gmail.com>
2023-10-03 12:00:00 +0900

    .

--- conv.sh
+++ conv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir trimmed
 for p in $* ; do
-    convert $p -trim -resize x630 +repage trimmed/$p
+    convert $p -trim -resize x630 +repage -fuzz 20% -transparent "#EC721A" trimmed/$p
 done
